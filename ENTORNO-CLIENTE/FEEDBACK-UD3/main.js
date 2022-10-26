fe2b7e23ee03fa72714ed4cec3114a9976c9f3fc
SSanchez15394 <100305556+SSanchez15394@users.noreply.github.com>
2022-10-26 12:20:21 +0200

    Delete ENTORNO-CLIENTE directory

--- ENTORNO-CLIENTE/FEEDBACK-UD3/main.js
+++
@@ -1,8 +0,0 @@
-function onKeyDown() {
-    document.getElementById("box").style.backgroundColor = "blue";
-}
-
-function onKeyUp() {
-   document.getElementById("box").style.backgroundColor = "red";
-}
-
